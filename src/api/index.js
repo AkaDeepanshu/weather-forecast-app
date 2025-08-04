3ff47327b3384b1b97d991cce256d3dffca40e51
Deepnshu Sharma <deepanshu0859@gmail.com>
2025-08-04 23:37:32 +0530

    final changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const getWeatherDataFromCity = async ( city )=>{
-    const response = await fetch(`${BASE_URL}&q=${city}&api=yes`)
+    const url = `${BASE_URL}&q=${city}&api=yes`;
+    const response = await fetch(url)
     return response.json();
 }
 
